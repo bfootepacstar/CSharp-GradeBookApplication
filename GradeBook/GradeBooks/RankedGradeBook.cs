bd507348cf8192fd0db21ab44826f8c4191656c3
Unknown <bfootepacstar@gmail.com>
2018-12-13 10:28:05 -0800

    Overrode RankedGradeBook's GetLetterGrade
    Let's hope it's all correct on the first push.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,25 @@ namespace GradeBook.GradeBooks
 		{
 			Type = GradeBookType.Ranked;
 		}
-    }
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5)
+				throw new InvalidOperationException("Ranked grading requires a minimum of 5 students.");
+
+			if (averageGrade >= 80.0)
+				return 'A';
+
+			if ((averageGrade < 80.0) && (averageGrade >= 60.0))
+				return 'B';
+
+			if ((averageGrade < 60.0) && (averageGrade >= 40.0))
+				return 'C';
+
+			if ((averageGrade < 40.0) && (averageGrade >= 20.0))
+				return 'D';
+
+			return 'F';
+		}
+	}
 }
